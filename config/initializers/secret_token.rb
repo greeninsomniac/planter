a63707d14f30ef18935dca2d345a30fc6b916bef
Matthew Castle <matt@flukyrobot.com>
2013-02-09 10:03:23 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Planter::Application.config.secret_token = '4af1b2e249e2ed0a2ef6adf854b1822140d86bf0a4b200732e2a202c538a69537b3a34b1d4c835a7b5fccbb1f414de371ef9c3268e8e90f301d1835043812ac0'
